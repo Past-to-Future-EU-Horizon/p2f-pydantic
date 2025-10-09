7d4e7801caff9bcb09f425ebe077700aef7bf423
Garrett Speed <g.t.speed@uu.nl>
2025-10-09 19:43:49 +0200

    Sync datasets with API definition

--- p2f_pydantic/datasets.py
+++ p2f_pydantic/datasets.py
@@ -8,9 +8,12 @@ class Datasets(BaseModel):
     dataset_identifier: Optional[UUID] = None
     doi: str
     title: str
+    sub_dataset_name: Optional[str] = None
     publication_date: date
-    is_new_p2f: bool
+    is_new_p2f: bool = False
+    is_sub_dataset: bool = False
 
+    
 ## dataset_datacite not included here as it will be an API server side 
 ## collected resource only, and will be downloaded as a JSON or XML
 ## literal, not parsed into a proper table. This can change in the 
