c5ebeb16671a206aa6ae18dec96715c27ca7e394
Garrett Speed <g.t.speed@uu.nl>
2025-10-09 18:40:44 +0200

    update __init__ to module

---
+++ p2f_pydantic/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["settings", 
+           "datasets", 
+           "harm_data_metadata", 
+           "harm_data_numerical",
+           "harm_data_record", 
+           "harm_data_types"]
