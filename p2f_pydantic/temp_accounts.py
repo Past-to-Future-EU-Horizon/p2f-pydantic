15f9bd18b7dba8e6ecfc7a0db9c926c924e7cc33
Garrett Speed <g.t.speed@uu.nl>
2025-10-23 15:42:59 +0200

    Add temp account data model

---
+++ p2f_pydantic/temp_accounts.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from pydantic import EmailStr
+from datetime import datetime
+
+class temp_accounts(BaseModel):
+    email: EmailStr
+    token: str
+    expiration: datetime
