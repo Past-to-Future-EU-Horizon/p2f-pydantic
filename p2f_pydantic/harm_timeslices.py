09bb51b7a4704d68ff6daf285afe59fface10739
Garrett Speed <g.t.speed@uu.nl>
2025-10-23 12:56:09 +0200

    Add HARM Timeslice data model

---
+++ p2f_pydantic/harm_timeslices.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import Optional
+from uuid import UUID
+
+class harm_timeslice(BaseModel):
+    timeslice_id: Optional[UUID] = None
+    timeslice_name: str
+    timeslice_age_mean = int
+    timeslice_age_recent = Optional[int]
+    timeslice_age_oldest = Optional[int]
