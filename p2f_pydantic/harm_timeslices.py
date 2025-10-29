04a5cc7491eaeac02ff4a9e2411fe20c064bfbf9
Garrett Speed <g.t.speed@uu.nl>
2025-10-29 13:35:49 +0100

    Correction to timeslice data model

--- p2f_pydantic/harm_timeslices.py
+++ p2f_pydantic/harm_timeslices.py
@@ -5,6 +5,6 @@ from uuid import UUID
 class harm_timeslice(BaseModel):
     timeslice_id: Optional[UUID] = None
     timeslice_name: str
-    timeslice_age_mean = int
-    timeslice_age_recent = Optional[int]
-    timeslice_age_oldest = Optional[int]
+    timeslice_age_mean: int
+    timeslice_age_recent: Optional[int]
+    timeslice_age_oldest: Optional[int]
