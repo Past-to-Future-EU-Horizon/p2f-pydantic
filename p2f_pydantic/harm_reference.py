250405d52e2a3553d2289fe0b1d98ff896423a85
Garrett Speed <g.t.speed@uu.nl>
2025-11-13 16:20:42 +0100

    Add harm reference

---
+++ p2f_pydantic/harm_reference.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from pydantic import HttpUrl
+from typing import Optional
+from uuid import UUID
+
+def harm_reference(BaseModel):
+    reference_id: Optional[UUID] = None
+    doi: Optional[HttpUrl] = None
+    other_link: Optional[HttpUrl] = None
+    reference_type: Optional[str] = None
+    reference_content: Optional[str] = None
