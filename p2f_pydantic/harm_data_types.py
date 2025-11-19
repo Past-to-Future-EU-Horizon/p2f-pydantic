d5f43ddc95647d474488e094e43a52aeffd26092
Garrett Speed <g.t.speed@uu.nl>
2025-11-19 12:21:00 +0100

    Update data type to match new API definition Part of the effort to remove PKs from pydantic library

--- p2f_pydantic/harm_data_types.py
+++ p2f_pydantic/harm_data_types.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 from typing import Optional
+from uuid import UUID
 
 class harm_data_type(BaseModel):
+    datatype_id: Optional[UUID] = None
     measure: str
     unit_of_measurement: str
     method: Optional[str] = None
