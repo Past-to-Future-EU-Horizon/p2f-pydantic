2dfb61bf68bdb786a1c32dda197128b9a5712b2b
Garrett Speed <g.t.speed@uu.nl>
2026-02-10 10:40:44 +0100

    update harm_reference doi data type

--- p2f_pydantic/harm_reference.py
+++ p2f_pydantic/harm_reference.py
@@ -5,7 +5,8 @@ from uuid import UUID
 
 class harm_reference(BaseModel):
     reference_id: Optional[UUID] = None
-    doi: Optional[HttpUrl] = None
+    # Now a string because storing the 10prefix/suffix is probably better
+    doi: Optional[str] = None               
     other_link: Optional[HttpUrl] = None
     reference_type: Optional[str] = None
     reference_content: Optional[str] = None
