74847fe9c1529d0aa1195457187a787729940bea
Garrett Speed <g.t.speed@uu.nl>
2026-04-20 12:12:47 +0200

    Update System models to use uppercase in code style

--- p2f_pydantic/system.py
+++ p2f_pydantic/system.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel
 from typing import Optional
 
-class semantic_version(BaseModel):
+class Semantic_Version(BaseModel):
     major: int
     minor: int
     patch: int
 
-class api_metadata(BaseModel):
-    pyclient_minimum_version: Optional[semantic_version]
-    api_system_version: Optional[semantic_version]
+class API_Metadata(BaseModel):
+    pyclient_minimum_version: Optional[Semantic_Version] = None
+    api_system_version: Optional[Semantic_Version] = None
