ff7f57e253ca25768162f35b54d1cfe4476ade47
Garrett Speed <g.t.speed@uu.nl>
2026-05-07 14:14:15 +0200

    Fix __init__ issue

--- p2f_pydantic/__init__.py
+++ p2f_pydantic/__init__.py
@@ -1,11 +1,13 @@
 import p2f_pydantic.datasets
 import p2f_pydantic.harm_age
-import p2f_pydantic.harm_data_metadata
+import p2f_pydantic.harm_location
+import p2f_pydantic.harm_species
 import p2f_pydantic.harm_data_numerical
 import p2f_pydantic.harm_data_record
 import p2f_pydantic.harm_data_types
 import p2f_pydantic.harm_reference
 import p2f_pydantic.harm_timeslices
+import p2f_pydantic.harm_ds_time
 import p2f_pydantic.link_git
 import p2f_pydantic.data_quality
 import p2f_pydantic.system
@@ -16,12 +18,14 @@ import p2f_pydantic.doi
 __all__ = ["settings", 
            "datasets", 
            "harm_age",
-           "harm_data_metadata", 
+           "harm_location",
+           "harm_species", 
            "harm_data_numerical",
            "harm_data_record", 
            "harm_data_types", 
            "harm_reference",
            "harm_timeslices",
+           "harm_ds_time",
            "link_git", 
            "data_quality",
            "system",
