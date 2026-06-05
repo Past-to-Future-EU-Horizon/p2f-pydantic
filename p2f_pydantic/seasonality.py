8fcc869d16cec0d510da602bb6e5de7055c0cc87
Garrett Speed <g.t.speed@uu.nl>
2026-06-05 11:19:59 +0200

    update seasonailty

--- p2f_pydantic/seasonality.py
+++ p2f_pydantic/seasonality.py
@@ -4,6 +4,7 @@ from uuid import UUID
 
 class Seasonality_DS(BaseModel):
     model_config = ConfigDict(from_attributes=True)
+    dataset_id: UUID
     seasonality_type: Literal["Winter/Summer", 
                               "Spring/Summer/Fall/Winter", 
                               "Hot/Cold", 
@@ -12,6 +13,7 @@ class Seasonality_DS(BaseModel):
     
 class Season(BaseModel):
     model_config = ConfigDict(from_attributes=True)
+    record_hash: str
     season: Literal["Summer", 
                     "Spring", 
                     "Fall",
