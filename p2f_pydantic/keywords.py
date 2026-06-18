65026b93293ddfbd0013275762ca43c23f114f20
Garrett Speed <g.t.speed@uu.nl>
2026-06-18 17:24:17 +0200

    Update keywords to match new model

--- p2f_pydantic/keywords.py
+++ p2f_pydantic/keywords.py
@@ -1,14 +1,15 @@
 from pydantic import BaseModel, ConfigDict
 from typing import Optional, Literal
+from uuid import UUID
 
 class Keywords(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     keyword: str
-    taxon: Optional[str] = None
 
-
-class KeywordDictionary(BaseModel):
+class TaxonomicDict(BaseModel):
     model_config = ConfigDict(from_attributes=True)
+    taxdict_id: UUID
     keyword: str
-    taxon: Optional[str] = None
-    usage: int = 0
+    taxonomy: str
+    parent_keyword: Optional[str] = None
+    taxonomic_id: Optional[str] = None
