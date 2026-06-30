77481bc501c3594770c5358e34550c73c21e6dce
Garrett Speed <g.t.speed@uu.nl>
2026-06-30 09:24:41 +0200

    Fix HARM Timeslice model optionality

--- p2f_pydantic/harm_timeslices.py
+++ p2f_pydantic/harm_timeslices.py
@@ -6,6 +6,6 @@ class HARM_Timeslice(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     timeslice_id: Optional[UUID] = None
     timeslice_name: str
-    timeslice_age_mean: int
-    timeslice_age_recent: Optional[int]
-    timeslice_age_oldest: Optional[int]
+    timeslice_age_mean: Optional[int]=None
+    timeslice_age_recent: Optional[int]=None
+    timeslice_age_oldest: Optional[int]=None
